637f52e0c6e85518a483b138798877f7ba8d0ffd
yuanzhang <945947732@qq.com>
2023-09-13 23:24:50 +0800

    chore: xo => biome

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { pickRandomSurname, pickRandomWords } from './utils';
+import { pickRandomSurname, pickRandomWords } from "./utils";
 
 /**
  * 返回随机中文名
@@ -29,34 +29,34 @@ function cnname(surname: string): string;
 function cnname(surname: string, num: number): string[];
 
 function cnname(
-  parameter1?: number | string,
-  parameter2?: number,
+	parameter1?: number | string,
+	parameter2?: number,
 ): string | string[] {
-  let fixSurname;
-  let num;
-  let single = false;
-  if (typeof parameter1 === 'string') {
-    fixSurname = parameter1;
-    num = parameter2;
-  } else if (typeof parameter1 === 'number') {
-    num = parameter1;
-  }
+	let fixSurname;
+	let num;
+	let single = false;
+	if (typeof parameter1 === "string") {
+		fixSurname = parameter1;
+		num = parameter2;
+	} else if (typeof parameter1 === "number") {
+		num = parameter1;
+	}
 
-  if (num === undefined) {
-    num = 1;
-    single = true;
-  }
+	if (num === undefined) {
+		num = 1;
+		single = true;
+	}
 
-  const result = [];
-  while (result.length < num) {
-    const surname = fixSurname || pickRandomSurname();
-    const nameLength = Math.random() > 0.5 ? 2 : 1;
-    const name = pickRandomWords(nameLength);
-    const fullName = surname + name;
-    result.push(fullName);
-  }
+	const result = [];
+	while (result.length < num) {
+		const surname = fixSurname || pickRandomSurname();
+		const nameLength = Math.random() > 0.5 ? 2 : 1;
+		const name = pickRandomWords(nameLength);
+		const fullName = surname + name;
+		result.push(fullName);
+	}
 
-  return single ? result.toString() : result;
+	return single ? result.toString() : result;
 }
 
 export default cnname;
