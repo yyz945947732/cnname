fe4feb100be40ef58870efc28103177541227382
yyz945947732 <945947732@qq.com>
2025-03-07 22:53:46 +0800

    chore: example option 更新

--- scripts/api/cnname.mjs
+++ scripts/api/cnname.mjs
@@ -17,4 +17,44 @@ export default [
     label: 'cnname("姚", 5)',
     value: cnname('姚', 5),
   },
+  {
+    label: 'cnname({ count: 5 })',
+    value: cnname({ count: 5 }),
+  },
+  {
+    label: 'cnname({ count: 5, unique: true, onlyRepeatedGivenName: true })',
+    value: cnname({ count: 5, unique: true, onlyRepeatedGivenName: true }),
+  },
+  {
+    label: 'cnname({ count: 5, surname: "张" })',
+    value: cnname({ count: 5, surname: "张" }),
+  },
+  {
+    label: 'cnname({ count: 5, surname: "张", givenNameLength: 2 })',
+    value: cnname({ count: 5, surname: "张", givenNameLength: 2 }),
+  },
+  {
+    label: 'cnname({ count: 5, unique: true, nameType: "surname" })',
+    value: cnname({ count: 5, unique: true, nameType: "surname" }),
+  },
+  {
+    label: 'cnname({ count: 5, unique: true, nameType: "givenName" })',
+    value: cnname({ count: 5, unique: true, nameType: "givenName" }),
+  },
+  {
+    label: 'cnname({ count: 5, unique: true, surnameType: "single", givenNameLength: 1 })',
+    value: cnname({ count: 5, unique: true, surnameType: "single", givenNameLength: 1 }),
+  },
+  {
+    label: 'cnname({ count: 5, unique: true, surnameType: "compound", givenNameLength: 1 })',
+    value: cnname({ count: 5, unique: true, surnameType: "compound", givenNameLength: 1 }),
+  },
+  {
+    label: 'cnname({ count: 5, unique: true, onlyCommonSurname: true })',
+    value: cnname({ count: 5, unique: true, onlyCommonSurname: true }),
+  },
+  {
+    label: 'cnname({ count: 5, unique: true, onlyCommonSurname: true, nameType: "surname" })',
+    value: cnname({ count: 5, unique: true, onlyCommonSurname: true, nameType: "surname" }),
+  },
 ];
