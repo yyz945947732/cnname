0c2b407e576d2d14faa2a939856aaffb4aa521df
yyz945947732 <945947732@qq.com>
2025-03-09 01:11:00 +0800

    style: lint code

--- src/lib/parseName.ts
+++ src/lib/parseName.ts
@@ -7,38 +7,37 @@ import { getIsCompoundSurname, getIsSingleCharacterSurname } from '../utils';
  * @returns {ParseNameResult} 解析结果
  */
 function parseName(name: string): ParseNameResult {
-    if (name.length === 0) {
-        return {
-            surname: '',
-            givenName: '',
-        };
-    }
-
-    if (name.length >= 3) {
-        const isCompoundSurname = getIsCompoundSurname(name.slice(0, 2));
-        if (isCompoundSurname) {
-            return {
-                surname: name.slice(0, 2),
-                givenName: name.slice(2),
-            };
-        }
-
-    }
-
-    const isSingleCharacterSurname = getIsSingleCharacterSurname(name.slice(0, 1));
-    if (isSingleCharacterSurname) {
-        return {
-            surname: name.slice(0, 1),
-            givenName: name.slice(1),
-        };
-    }
-
+  if (name.length === 0) {
     return {
-        surname: '',
-        givenName: name,
+      surname: '',
+      givenName: '',
     };
+  }
+
+  if (name.length >= 3) {
+    const isCompoundSurname = getIsCompoundSurname(name.slice(0, 2));
+    if (isCompoundSurname) {
+      return {
+        surname: name.slice(0, 2),
+        givenName: name.slice(2),
+      };
+    }
+  }
+
+  const isSingleCharacterSurname = getIsSingleCharacterSurname(
+    name.slice(0, 1),
+  );
+  if (isSingleCharacterSurname) {
+    return {
+      surname: name.slice(0, 1),
+      givenName: name.slice(1),
+    };
+  }
+
+  return {
+    surname: '',
+    givenName: name,
+  };
 }
 
 export default parseName;
-
-
