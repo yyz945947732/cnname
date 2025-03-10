bc7709fdf5cb8d6b4eb2216893f5a34898e9720a
yyz945947732 <945947732@qq.com>
2025-03-10 16:15:04 +0800

    chore: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    include: ['test'],
+    include: ['**/test/**'],
     coverage: {
       provider: 'istanbul',
       reporter: ['cobertura', 'lcov'],
