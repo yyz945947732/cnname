5a70496732b4d8ac81292454136451eebf8ff13a
yyz945947732 <945947732@qq.com>
2025-03-19 01:44:42 +0800

    styles: size.ts better code

--- src/utils/default.ts
+++ src/utils/default.ts
@@ -15,6 +15,5 @@ export const DEFAULT_SURNAME_ALGORITHM: Algorithm = 'weight';
 /**
  * @private
  * 名字属性的优先级顺序，决定如何筛选名字
- * `elements` (五行) > `gender` (性别)
  */
 export const GIVEN_NAME_TYPE_PRIORITY = ['elements', 'gender'] as const;
