25e3fab6022e1ac5f01bbdf9f7b1af04fe841a72
yyz945947732 <945947732@qq.com>
2025-03-26 16:18:08 +0800

    test: fix test case wrong spell

--- test/apis/cnname.test.ts
+++ test/apis/cnname.test.ts
@@ -6,24 +6,24 @@ assertType<string>(cnname('张'));
 assertType<string[]>(cnname(20));
 assertType<string[]>(cnname('张', 20));
 
-describe('cname', () => {
-  test('cname() should return string', () => {
+describe('cnname', () => {
+  test('cnname() should return string', () => {
     expect(typeof cnname()).toBe('string');
   });
-  test('cname(-1) should return array with one elements', () => {
+  test('cnname(-1) should return array with one elements', () => {
     expect(cnname(-1).length).toBe(0);
   });
-  test('cname(0) should return empty array', () => {
+  test('cnname(0) should return empty array', () => {
     expect(cnname(0).length).toBe(0);
   });
-  test('cname(5) should return array with five elements', () => {
+  test('cnname(5) should return array with five elements', () => {
     expect(cnname(5).length).toBe(5);
   });
-  test('cname("张") should return string', () => {
+  test('cnname("张") should return string', () => {
     expect(typeof cnname('张')).toBe('string');
     expect(cnname('张').startsWith('张')).toBeTruthy();
   });
-  test('cname("张", 5) should return array with five elements', () => {
+  test('cnname("张", 5) should return array with five elements', () => {
     expect(cnname('张', 5).length).toBe(5);
     expect(cnname('张', 5).every((name) => name.startsWith('张'))).toBeTruthy();
   });
