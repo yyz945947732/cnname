054717266724e172d0a4b5b4932b5c38635ee454
yyz945947732 <945947732@qq.com>
2025-03-31 22:13:56 +0800

    chore: add local script for dict.

--- scripts/utils/index.mjs
+++ scripts/utils/index.mjs
@@ -3,18 +3,27 @@ import { fileURLToPath } from 'node:url';
 import path from 'node:path';
 
 /**
+ * 更新文件夹对应文件
+ * @param {string} content 内容
+ * @param {string} filePath 文件路径
+*/
+export function updateFile(content, filePath) {
+  const __filename = fileURLToPath(import.meta.url);
+
+  const __dirname = path.dirname(__filename);
+
+  const resolvePath = path.resolve(__dirname, filePath);
+
+  writeFileSync(resolvePath, content, 'utf-8');
+
+  console.log(`${resolvePath} 更新成功！`);
+}
+
+/**
  * 更新 example 文件夹对应文件
  * @param {string} content 内容
  * @param {string} filename 文件名
  */
 export function updateExample(content, filename) {
-  const __filename = fileURLToPath(import.meta.url);
-
-  const __dirname = path.dirname(__filename);
-
-  const examplePath = path.resolve(__dirname, `../../example/${filename}.md`);
-
-  writeFileSync(examplePath, content, 'utf-8');
-
-  console.log(`${filename}.md 更新成功！`);
+  updateFile(content, `../../example/${filename}.md`);
 }
