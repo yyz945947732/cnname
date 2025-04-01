42deb19939c36e3a6aaab6f00c46a80ee8bf51a9
yyz945947732 <945947732@qq.com>
2025-04-01 17:54:44 +0800

    chore: perf case update.

--- performance/index.bench.ts
+++ performance/index.bench.ts
@@ -5,91 +5,67 @@ const TEST_LARGE_COUNT = 50000;
 const TEST_FEW_COUNT = 50;
 
 describe(`count ${TEST_LARGE_COUNT}`, () => {
-  bench('unique weight surname', () => {
-    cnname({ count: TEST_LARGE_COUNT, unique: true, part: 'surname', surnameAlgorithm: 'weight' });
-  });
-  bench('unique random surname', () => {
-    cnname({ count: TEST_LARGE_COUNT, unique: true, part: 'surname', surnameAlgorithm: 'random' });
-  });
-  bench('unique weight fullname', () => {
-    cnname({ count: TEST_LARGE_COUNT, unique: true, surnameAlgorithm: 'weight' });
-  });
-  bench('unique random fullname', () => {
-    cnname({ count: TEST_LARGE_COUNT, unique: true, surnameAlgorithm: 'random' });
-  });
   bench('weight surname', () => {
     cnname({ count: TEST_LARGE_COUNT, part: 'surname', surnameAlgorithm: 'weight' });
   });
   bench('random surname', () => {
     cnname({ count: TEST_LARGE_COUNT, part: 'surname', surnameAlgorithm: 'random' });
   });
-  bench('weight fullname', () => {
-    cnname({ count: TEST_LARGE_COUNT, surnameAlgorithm: 'weight' });
-  });
-  bench('random fullname', () => {
-    cnname({ count: TEST_LARGE_COUNT, surnameAlgorithm: 'random' });
-  });
-  bench('unique givenName', () => {
-    cnname({ count: TEST_LARGE_COUNT, unique: true, part: 'givenName' });
-  });
   bench('givenName', () => {
-    cnname({ count: TEST_LARGE_COUNT, part: 'givenName' });
-  });
-  bench('givenName len 1', () => {
-    cnname({ count: TEST_LARGE_COUNT, part: 'givenName', givenNameLength: 1 });
-  });
-  bench('givenName len 2', () => {
     cnname({ count: TEST_LARGE_COUNT, part: 'givenName', givenNameLength: 2 });
   });
-  bench('unique givenName len 1', () => {
-    cnname({ count: TEST_LARGE_COUNT, unique: true, part: 'givenName', givenNameLength: 1 });
+  bench('weight fullname', () => {
+    cnname({ count: TEST_LARGE_COUNT, givenNameLength: 2, surnameAlgorithm: 'weight' });
   });
-  bench('unique givenName len 2', () => {
+  bench('random fullname', () => {
+    cnname({ count: TEST_LARGE_COUNT, givenNameLength: 2, surnameAlgorithm: 'random' });
+  });
+  bench('unique weight surname', () => {
+    cnname({ count: TEST_LARGE_COUNT, unique: true, part: 'surname', surnameAlgorithm: 'weight' });
+  });
+  bench('unique random surname', () => {
+    cnname({ count: TEST_LARGE_COUNT, unique: true, part: 'surname', surnameAlgorithm: 'random' });
+  });
+  bench('unique givenName', () => {
     cnname({ count: TEST_LARGE_COUNT, unique: true, part: 'givenName', givenNameLength: 2 });
+  });
+  bench('unique weight fullname', () => {
+    cnname({ count: TEST_LARGE_COUNT, unique: true, givenNameLength: 2, surnameAlgorithm: 'weight' });
+  });
+  bench('unique random fullname', () => {
+    cnname({ count: TEST_LARGE_COUNT, unique: true, givenNameLength: 2, surnameAlgorithm: 'random' });
   });
 });
 
 describe(`count ${TEST_FEW_COUNT}`, () => {
-  bench('unique weight surname', () => {
-    cnname({ count: TEST_FEW_COUNT, unique: true, part: 'surname', surnameAlgorithm: 'weight' });
-  });
-  bench('unique random surname', () => {
-    cnname({ count: TEST_FEW_COUNT, unique: true, part: 'surname', surnameAlgorithm: 'random' });
-  });
-  bench('unique weight fullname', () => {
-    cnname({ count: TEST_FEW_COUNT, unique: true, surnameAlgorithm: 'weight' });
-  });
-  bench('unique random fullname', () => {
-    cnname({ count: TEST_FEW_COUNT, unique: true, surnameAlgorithm: 'random' });
-  });
   bench('weight surname', () => {
     cnname({ count: TEST_FEW_COUNT, part: 'surname', surnameAlgorithm: 'weight' });
   });
   bench('random surname', () => {
     cnname({ count: TEST_FEW_COUNT, part: 'surname', surnameAlgorithm: 'random' });
   });
-  bench('weight fullname', () => {
-    cnname({ count: TEST_FEW_COUNT, surnameAlgorithm: 'weight' });
-  });
-  bench('random fullname', () => {
-    cnname({ count: TEST_FEW_COUNT, surnameAlgorithm: 'random' });
-  });
-  bench('unique givenName', () => {
-    cnname({ count: TEST_FEW_COUNT, unique: true, part: 'givenName' });
-  });
   bench('givenName', () => {
-    cnname({ count: TEST_FEW_COUNT, part: 'givenName' });
-  });
-  bench('givenName len 1', () => {
-    cnname({ count: TEST_FEW_COUNT, part: 'givenName', givenNameLength: 1 });
-  });
-  bench('givenName len 2', () => {
     cnname({ count: TEST_FEW_COUNT, part: 'givenName', givenNameLength: 2 });
   });
-  bench('unique givenName len 1', () => {
-    cnname({ count: TEST_FEW_COUNT, unique: true, part: 'givenName', givenNameLength: 1 });
+  bench('weight fullname', () => {
+    cnname({ count: TEST_FEW_COUNT, givenNameLength: 2, surnameAlgorithm: 'weight' });
   });
-  bench('unique givenName len 2', () => {
+  bench('random fullname', () => {
+    cnname({ count: TEST_FEW_COUNT, givenNameLength: 2, surnameAlgorithm: 'random' });
+  });
+  bench('unique weight surname', () => {
+    cnname({ count: TEST_FEW_COUNT, unique: true, part: 'surname', surnameAlgorithm: 'weight' });
+  });
+  bench('unique random surname', () => {
+    cnname({ count: TEST_FEW_COUNT, unique: true, part: 'surname', surnameAlgorithm: 'random' });
+  });
+  bench('unique givenName', () => {
     cnname({ count: TEST_FEW_COUNT, unique: true, part: 'givenName', givenNameLength: 2 });
+  });
+  bench('unique weight fullname', () => {
+    cnname({ count: TEST_FEW_COUNT, unique: true, givenNameLength: 2, surnameAlgorithm: 'weight' });
+  });
+  bench('unique random fullname', () => {
+    cnname({ count: TEST_FEW_COUNT, unique: true, givenNameLength: 2, surnameAlgorithm: 'random' });
   });
 });
