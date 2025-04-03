fcaf4530032266f9e061b138a061abc66e6af3eb
yyz945947732 <945947732@qq.com>
2025-04-04 00:53:15 +0800

    test: add test case for `getGivenName`, `getSurname`.

--- test/apis/getGivenName.test.ts
+++ test/apis/getGivenName.test.ts
@@ -3,6 +3,8 @@ import { assertType, describe, expect, test } from 'vitest';
 
 assertType<string>(getGivenName());
 assertType<string[]>(getGivenName(1));
+assertType<string[]>(getGivenName(0));
+assertType<string[]>(getGivenName(-1));
 
 describe('getGivenName', () => {
   test('getGivenName() should return a string', () => {
@@ -11,5 +13,13 @@ describe('getGivenName', () => {
   test('getGivenName(5) should return array with five elements', () => {
     expect(getGivenName(5)).toBeInstanceOf(Array);
     expect(getGivenName(5)).toHaveLength(5);
+  });
+  test('getGivenName(0) should return empty array', () => {
+    expect(getGivenName(0)).toBeInstanceOf(Array);
+    expect(getGivenName(0)).toHaveLength(0);
+  });
+  test('getGivenName(-1) should return empty array', () => {
+    expect(getGivenName(-1)).toBeInstanceOf(Array);
+    expect(getGivenName(-1)).toHaveLength(0);
   });
 });
--- test/apis/getSurname.test.ts
+++ test/apis/getSurname.test.ts
@@ -3,6 +3,8 @@ import { assertType, describe, expect, test } from 'vitest';
 
 assertType<string>(getSurname());
 assertType<string[]>(getSurname(1));
+assertType<string[]>(getSurname(0));
+assertType<string[]>(getSurname(-1));
 
 describe('getSurname', () => {
   test('getSurname() should return a string', () => {
@@ -11,5 +13,13 @@ describe('getSurname', () => {
   test('getSurname(5) should return array with one elements', () => {
     expect(getSurname(5)).toBeInstanceOf(Array);
     expect(getSurname(5)).toHaveLength(5);
+  });
+  test('getSurname(0) should return empty array', () => {
+    expect(getSurname(0)).toBeInstanceOf(Array);
+    expect(getSurname(0)).toHaveLength(0);
+  });
+  test('getSurname(-1) should return empty array', () => {
+    expect(getSurname(-1)).toBeInstanceOf(Array);
+    expect(getSurname(-1)).toHaveLength(0);
   });
 });
